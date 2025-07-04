075709b21c58ab5469bddfc3c0cc0e5a76a57a84
Alejandro Gutierrez <c.gutierrez.d.alejandro@gmail.com>
2025-07-03 20:56:22 -0500

    add table reports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { ChartAreaInteractive } from "@/features/home/components/Graphics"
+import TableReports from "@/features/home/components/Reports"
 import TotalIncome from "@/features/home/components/TotalIncome"
 import UsersTotalsCards from "@/features/home/components/Totals"
 import Welcome from "@/features/home/components/Welcome"
@@ -11,6 +12,9 @@ const Homepage = () => {
       <ChartAreaInteractive />
       <div className="flex -mt-100 ml-192">
         <TotalIncome />
+      </div>
+      <div className="flex justify-center items-center w-240 border rounded-xl ml-9 mt-8 p-4">
+        <TableReports />
       </div>
     </div>
   )
