155d542672705fb66154107f0f4367cd246ccea1
Andrés Gutiérrez <g.gutierrez.j.andres@gmail.com>
2025-07-16 20:33:48 +0200

    feat: build static prohect

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [new URL("https://pokeapi.co/**")],
-  },
-};
+  output: "export",
+  basePath: "",
+  assetPrefix: "",
+}
 
-export default nextConfig;
+export default nextConfig
