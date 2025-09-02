2a78c6017880616b0e69603226642161b4811c5b
Alejandro Gutierrez <c.gutierrez.d.alejandro@gmail.com>
2025-09-02 15:35:46 -0500

    build fix cards home and users

--- src/features/users/components/MetricsCardsList/index.tsx
+++ src/features/users/components/MetricsCardsList/index.tsx
@@ -20,7 +20,7 @@ const MetricsCardsList = ({ rango = "mes" }: MetricsCardsListProps) => {
   // Loading state
   if (loading) {
     return (
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-100">
         {[1, 2, 3].map((index) => (
           <div
             key={index}
